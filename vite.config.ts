8897581ec8bb52c1504629dd9fb08294a8f0ce1c
Joachim Martinsen <joachim.martinsen@gmail.com>
2026-08-12 23:05:00 +0200

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/pokerprize/",
   plugins: [react()],
   test: {
     include: ["src/**/*.test.ts"],
